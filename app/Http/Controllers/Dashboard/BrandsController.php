a21b223ba361e05b9e5bf1f6b4801f7d3db9dacd
yasmeen <yasmeen1991h@gmail.com>
2020-09-16 14:28:17 +0300

    Categories CRUD

---
+++ app/Http/Controllers/Dashboard/BrandsController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BrandRequest;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BrandsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $brands = Brand::orderBy('id','desc')->paginate(PAGINATION_COUNT);
+        return view('dashboard.brands.index', compact('brands'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard.brands.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BrandRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            //validation
+
+            if (!$request->has('is_active'))
+                $request->request->add(['is_active' => 0]);
+            else
+                $request->request->add(['is_active' => 1]);
+
+
+            $fileName = "";
+            if ($request->has('photo')) {
+
+                $fileName = uploadImage('brands', $request->photo);
+            }
+
+            $brand = Brand::create($request->except('_token', 'photo'));
+
+            //save translations
+            $brand->name = $request->name;
+            $brand->photo = $fileName;
+
+            $brand->save();
+            DB::commit();
+            return redirect()->route('admin.brands')->with(['success' => 'تم ألاضافة بنجاح']);
+
+        }catch (\Exception $ex){
+            DB::rollback();
+            return redirect()->route('admin.brands')->with(['error' => 'حدث خطا ما برجاء المحاوله لاحقا']);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //get specific brands and its translations
+        $brand = Brand::find($id);
+
+        if (!$brand)
+            return redirect()->route('admin.brands')->with(['error' => 'هذا الماركة غير موجود ']);
+
+        return view('dashboard.brands.edit', compact('brand'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function update(BrandRequest $request, $id)
+    {
+        try {
+            //validation
+
+            //update DB
+
+
+            $brand = Brand::find($id);
+
+            if (!$brand)
+                return redirect()->route('admin.brands')->with(['error' => 'هذا الماركة غير موجود']);
+
+
+            DB::beginTransaction();
+            if ($request->has('photo')) {
+                $fileName = uploadImage('brands', $request->photo);
+                Brand::where('id', $id)
+                    ->update([
+                        'photo' => $fileName,
+                    ]);
+            }
+
+            if (!$request->has('is_active'))
+                $request->request->add(['is_active' => 0]);
+            else
+                $request->request->add(['is_active' => 1]);
+
+            $brand->update($request->except('_token', 'id', 'photo'));
+
+            //save translations
+            $brand->name = $request->name;
+            $brand->save();
+
+            DB::commit();
+            return redirect()->route('admin.brands')->with(['success' => 'تم ألتحديث بنجاح']);
+
+        } catch (\Exception $ex) {
+
+            DB::rollback();
+            return redirect()->route('admin.brands')->with(['error' => 'حدث خطا ما برجاء المحاوله لاحقا']);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            //get specific categories and its translations
+            $brand = Brand::find($id);
+
+            if (!$brand)
+                return redirect()->route('admin.brands')->with(['error' => 'هذا الماركة غير موجود ']);
+
+            $brand->delete();
+
+            return redirect()->route('admin.brands')->with(['success' => 'تم  الحذف بنجاح']);
+
+        } catch (\Exception $ex) {
+            return redirect()->route('admin.brands')->with(['error' => 'حدث خطا ما برجاء المحاوله لاحقا']);
+        }
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -58,7 +58,19 @@ Route::group(
             Route::get('delete/{id}','SubCategoriesController@destroy') -> name('admin.subcategories.delete');
         });
 
-        ################################## end categories    #######################################
+        ################################## end sub categories    #######################################
+
+
+        ################################## brands routes ######################################
+        Route::group(['prefix' => 'brands'], function () {
+            Route::get('/','BrandsController@index') -> name('admin.brands');
+            Route::get('create','BrandsController@create') -> name('admin.brands.create');
+            Route::post('store','BrandsController@store') -> name('admin.brands.store');
+            Route::get('edit/{id}','BrandsController@edit') -> name('admin.brands.edit');
+            Route::post('update/{id}','BrandsController@update') -> name('admin.brands.update');
+            Route::get('delete/{id}','BrandsController@destroy') -> name('admin.brands.delete');
+        });
+        ################################## end brands    #######################################
     });
 
 
